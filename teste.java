9bb2355bb8e12b3ad619d4d21063708d500e9ff5
Eduardo Ukstin <ukstin@gmail.com>
2021-05-26 17:30:22 -0300

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+
+        int x, aux = 0;
+
+        x = teclado.nextInt();
+        if ( x > 5 && x <= 2000 ) {
+            for ( int i = 2; i <= x; i+=2 ) {
+                aux = i * i;
+                System.out.printf("%d^2 = %d\n",i,aux);
+            }
+        }
+    }
+}
